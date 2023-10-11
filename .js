19d3f6a4d1443751966cd015d289714cea624c3a
Isabele Souza <isabele.engmec@gmail.com>
2023-10-11 20:17:03 -0300

    add chan

---
+++ .js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desselecionarBotao();
+    removerPersonagem();
+
+    botao.classList.add("selecionado");
+    personagens[indice].classList.add("selecionado");
+  });
+});
+function desselecionarBotao() {
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado.classList.remove("selecionado");
+}
+
+function removerPersonagem() {
+  const personagemSelecionado = document.querySelector(
+    ".personagem.selecionado"
+  );
+  personagemSelecionado.classList.remove("selecionado");
+}
